9d20d2a70234fb30f9b05eddae3f10bb3fedbd0f
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-03-01 20:15:13 +0300

    add Scene

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
+import scene from './scene.bin';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={scene} className="App-logo" alt="Scene" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org"  target="_blank" rel="noopener noreferrer">
+          Изучайте реакт и возвращайтесь к нам.
         </a>
       </header>
     </div>
   );
 }
+
+import {Suspense) from "react";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls) from "@react-three/drei";
+import Scene from "./models/Scene";
+const App = () => (
+    <Canvas camera={{position: [0, 2, 5], zoom: 1}}>
+      <OrbitControls />
+      <color attach="background" args={["lightblue"]} />
+      <hemisphereLight intensity={0.35} />
+      <spotLight
+            position={[10, 10, 10]}
+            angle={0.3}
+            penumbra={1}
+            intensity={2}
+            castShadow
+        />
+
+        <Suspense fallback={null}>
+        <Scene/>
+        </Suspense>
+        </Canvas>
+        );
 
 export default App;
